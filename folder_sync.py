3dab8dbaee70cdda1eaaeab2b70db3dbda95a362
Caleb Danielsen <csdigital2022@gmail.com>
2024-07-04 10:12:40 +0200

    add remove non-synch folders and files from dst folder

--- folder_sync.py
+++ folder_sync.py
@@ -30,7 +30,7 @@ class FolderSync:
             self.logger.error(f"Source directory '{self.src_folder}' does not exist.")
             return
         
-        # Synchrnoize from the source folder to the destination folder direction
+        # Synchronize from the source folder to the destination folder direction
         for src_dir, _, files in os.walk(self.src_folder):
             # Ensure 1 replacement only
             dst_dir = src_dir.replace(str(self.src_folder), str(self.dst_folder), 1)
@@ -51,6 +51,29 @@ class FolderSync:
                 if not os.path.exists(dst_file) or os.path.getmtime(src_file) is not os.path.getmtime(dst_file):
                     shutil.copy2(src_file, dst_file)
                     self.logger.info(f"File copied: {src_file} to {dst_file}")
+                    
+        # Remove any files or folders in the destination folder that are not in the source folder
+        for dst_dir, dirs, files in os.walk(self.dst_folder):
+            # Ensure 1 replacement only
+            src_dir = dst_dir.replace(str(self.dst_folder), str(self.src_folder), 1)
+            
+            for file in files:
+                src_file = os.path.join(src_dir, file)
+                dst_file = os.path.join(dst_dir, file)
+                
+                # Remove the file if it does not exist in the source folder
+                if not os.path.exists(src_file):
+                    os.remove(dst_file)
+                    self.logger.info(f"File removed: {dst_file}")
+                    
+            for dir in dirs:
+                src_sub_dir = os.path.join(src_dir, dir)
+                dst_sub_dir = os.path.join(dst_dir, dir)
+                
+                # Remove the folder if it does not exist in the source folder
+                if not os.path.exists(src_sub_dir):
+                    shutil.rmtree(dst_sub_dir)
+                    self.logger.info(f"Folder removed: {dst_sub_dir}")
             
 if __name__ == "__main__":
     src_folder = Path("src")
